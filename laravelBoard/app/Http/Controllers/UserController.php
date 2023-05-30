bf004d96c4f42b5a8caa02b3a8c52bfba4ee9e15
SNOH <116703016+snoh6839@users.noreply.github.com>
2023-05-30 15:27:35 +0900

    history

--- laravelBoard/app/Http/Controllers/UserController.php
+++ laravelBoard/app/Http/Controllers/UserController.php
@@ -1,5 +1,12 @@
 <?php
 
+/************************************************
+ * Project Name : laravel_board
+ * Directory    : Controllers
+ * File Name    : UserController.php
+ * History      : v001 0530 SNoh Create
+ *************************************************/
+
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
