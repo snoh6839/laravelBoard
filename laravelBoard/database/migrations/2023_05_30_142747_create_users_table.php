fda4d58db3a4864a9bc2a4ab2520253f898e0b61
SNOH <116703016+snoh6839@users.noreply.github.com>
2023-05-30 14:47:59 +0900

    php artisan migrate

--- laravelBoard/database/migrations/2023_05_30_142747_create_users_table.php
+++ laravelBoard/database/migrations/2023_05_30_142747_create_users_table.php
@@ -15,7 +15,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('email')->unique();
+            $table->string('name');
+            $table->string('password');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
