17fbca5a7b29daa0141c6eecb54cfdf37b77f611
SNOH <116703016+snoh6839@users.noreply.github.com>
2023-06-01 15:28:56 +0900

    assertViewIs

--- laravelBoard/tests/Feature/BoardsTest.php
+++ laravelBoard/tests/Feature/BoardsTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -21,5 +22,26 @@ class BoardsTest extends TestCase
         $response = $this->get('/boards');
 
         $response->assertRedirect('/users/login');
+    }
+
+    public function test_index_user_auth()
+    {
+        $user = new User([
+            'email' => 'aa@aa.aa'
+            ,'name' => '성이름'
+            ,'password' => '*Password123'
+        ]);
+        $user->save();
+        $response = $this->actingAs($user)->get('/boards');
+        $this->assertAuthenticatedAs($user);
+    }
+    public function test_index_user_auth_return_view()
+    {
+        $user = new User([
+            'email' => 'aa@aa.aa', 'name' => '성이름', 'password' => '*Password123'
+        ]);
+        $user->save();
+        $response = $this->actingAs($user)->get('/boards');
+        $response->assertViewIs('list');
     }
 }
