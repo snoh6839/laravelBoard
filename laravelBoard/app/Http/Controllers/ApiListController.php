05d5e6be2b7d64e9ba0c739e5bb8d360d0bb5b8c
SNOH <116703016+snoh6839@users.noreply.github.com>
2023-06-01 10:38:09 +0900

    validator add

--- laravelBoard/app/Http/Controllers/ApiListController.php
+++ laravelBoard/app/Http/Controllers/ApiListController.php
@@ -3,63 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Boards;
-
-class ApiListController extends Controller
-{
-    function getlist($id)
-    {
-        $board = Boards::find($id);
-        return response()->json([$board], 200);
-    }
-
-    function postlist(Request $req)
-    {
-        // 유효성 체크 필요
-
-        $boards = new Boards([
-            'title' => $req->title, 'content' => $req->content
-        ]);
-        $boards->save();
-
-        $arr['errorcode'] = '0';
-        $arr['msg'] = 'success';
-        $arr['data'] = $boards->only('id', 'title');
-
-        return $arr;
-    }
-
-    function putlist(Request $req, $id)
-    {
-        $boards = Boards::find($id);
-        $boards->title = $req->title;
-        $boards->content = $req->content;
-        $boards->save();
-
-        $arr['errorcode'] = '0';
-        $arr['msg'] = 'success';
-        $arr['data'] = $boards->only('id', 'title');
-
-        return $arr;
-    }
-
-    function deletelist($id)
-    {
-        $board = Boards::find($id);
-        $board->delete();
-
-        $arr['errorcode'] = '0';
-        $arr['msg'] = 'success';
-
-        return $arr;
-    }
-}
-
-<?php
-
-namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use App\Models\Boards;
 
