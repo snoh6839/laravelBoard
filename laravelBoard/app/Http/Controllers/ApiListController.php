ea2cac481aff1d8f98f8404d0646c3c586cf0590
SNOH <116703016+snoh6839@users.noreply.github.com>
2023-06-01 10:36:11 +0900

    api

--- laravelBoard/app/Http/Controllers/ApiListController.php
+++ laravelBoard/app/Http/Controllers/ApiListController.php
@@ -29,52 +29,8 @@ class ApiListController extends Controller
         return $arr;
     }
 
-    function putlist()
-    {
-        
-    }
-
-    function deletelist()
-    {
-        
-    }
-}
-
-<?php
-
-namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
-use App\Models\Boards;
-
-class ApiListController extends Controller
-{
-    function getlist($id)
-    {
-        $board = Boards::find($id);
-        return response()->json([$board], 200);
-    }
-
-    function postlist(Request $req)
-    {
-        // 유효성 체크 필요
-
-        $boards = new Boards([
-            'title' => $req->title, 'content' => $req->content
-        ]);
-        $boards->save();
-
-        $arr['errorcode'] = '0';
-        $arr['msg'] = 'success';
-        $arr['data'] = $boards->only('id', 'title');
-
-        return $arr;
-    }
-
     function putlist(Request $req, $id)
     {
-        // 유효성 체크 필요
-
         $boards = Boards::find($id);
         $boards->title = $req->title;
         $boards->content = $req->content;
@@ -94,9 +50,9 @@ class ApiListController extends Controller
 
         $arr['errorcode'] = '0';
         $arr['msg'] = 'success';
-        $arr['data'] = null;
 
         return $arr;
     }
 }
+
 
