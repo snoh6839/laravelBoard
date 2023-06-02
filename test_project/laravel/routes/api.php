883a2f0f3fd93ac4ac6a4cf26848e23157babfa0
SNOH <116703016+snoh6839@users.noreply.github.com>
2023-06-02 16:53:27 +0900

    php artisan make:controller ApiUserController

--- test_project/laravel/routes/api.php
+++ test_project/laravel/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ApiUserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/users/{id}', [ApiListController::class, 'userget']);
+Route::post('/users', [ApiListController::class, 'userpost']);
+Route::put('/users/{id}', [ApiListController::class, 'userput']);
+Route::delete('/users/{id}', [ApiListController::class, 'userdelete']);
