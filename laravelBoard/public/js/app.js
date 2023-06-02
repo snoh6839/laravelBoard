2a24f3a480f3d23bd5d244b39866933153bf7700
SNOH <116703016+snoh6839@users.noreply.github.com>
2023-06-02 11:54:42 +0900

    nav fix

--- laravelBoard/public/js/app.js
+++ laravelBoard/public/js/app.js
@@ -1,25 +1,28 @@
-require('./bootstrap');
+document.addEventListener('DOMContentLoaded', function () {
+    const indicator = document.querySelector('.nav-indicator');
+    const items = document.querySelectorAll('.nav-item');
 
+    function handleIndicator(el) {
+        items.forEach(item => {
+            item.classList.remove('is-active');
+            item.removeAttribute('style');
+        });
 
-const indicator = document.querySelector('.nav-indicator');
-const items = document.querySelectorAll('.nav-item');
+        indicator.style.width = `${el.offsetWidth}px`;
+        indicator.style.left = `${el.offsetLeft}px`;
+        indicator.style.backgroundColor = el.getAttribute('active-color');
 
-function handleIndicator(el) {
-    items.forEach(item => {
-        item.classList.remove('is-active');
-        item.removeAttribute('style');
+        el.classList.add('is-active');
+        el.style.color = el.getAttribute('active-color');
+    }
+
+    items.forEach((item, index) => {
+        item.addEventListener('click', e => {
+            handleIndicator(e.target);
+        });
+
+        if (item.classList.contains('is-active')) {
+            handleIndicator(item);
+        }
     });
-
-    indicator.style.width = `${el.offsetWidth}px`;
-    indicator.style.left = `${el.offsetLeft}px`;
-    indicator.style.backgroundColor = el.getAttribute('active-color');
-
-    el.classList.add('is-active');
-    el.style.color = el.getAttribute('active-color');
-}
-
-
-items.forEach((item, index) => {
-    item.addEventListener('click', e => { handleIndicator(e.target); });
-    item.classList.contains('is-active') && handleIndicator(item);
 });
--- laravelBoard/resources/views/layout/header.blade.php
+++ laravelBoard/resources/views/layout/header.blade.php
@@ -1,27 +1,27 @@
 <header>
     <h1>@yield('header', 'header')</h1>
-    
 </header>
-    @auth
-    <nav class="nav">
-        <a href="{{route('users.logout')}}" class="nav-item is-active" active-color="orange">로그아웃</a>
-        <a href="{{route('users.edit')}}" class="nav-item" active-color="green">회원정보 수정</a>
-        <span class="nav-indicator"></span>
-        <script src="{{asset('/js/app.js')}}"></script>
 
-    </nav>
-
-    @endauth
-    @guest
-    <nav class="nav">
-        <a href="{{route('users.login')}}" class="nav-item is-active" active-color="red">로그인 페이지</a>
+@auth
+<nav class="nav">
+    <a href="{{ route('users.logout') }}" class="nav-item {{ Request::is('users/edit') ? '' : 'is-active' }}" active-color="orange">로그아웃</a>
 
 
-        <a href="{{route('users.registration')}}" class="nav-item" active-color="blue">회원가입</a>
-        <span class="nav-indicator"></span>
-        <script src="{{asset('/js/app.js')}}"></script>
 
-    </nav>
-    @endguest
+    <a href="{{ route('users.edit') }}" class="nav-item {{ Request::is('users/edit') ? 'is-active' : '' }}" active-color="green">회원정보 수정</a>
 
+    <span class="nav-indicator"></span>
+</nav>
+@endauth
 
+@guest
+<nav class="nav">
+    <a href="{{ route('users.login') }}" class="nav-item {{ Request::is('users/login') ? 'is-active' : '' }}" active-color="red">로그인 페이지</a>
+
+    <a href="{{ route('users.registration') }}" class="nav-item {{ Request::is('users/registration') ? 'is-active' : '' }}" active-color="blue">회원가입</a>
+
+    <span class="nav-indicator"></span>
+</nav>
+@endguest
+
+<script src="{{ asset('/js/app.js') }}"></script>
